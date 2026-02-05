299a9bcd9ccd96e39ddaef9f296034cba77a193e
Suzuki <suzukiyumiotjiwarongo@gmail.com>
2026-02-05 21:01:13 +0200

    top5 new format

--- script.js
+++ script.js
@@ -532,7 +532,7 @@ async function fetchRanking(type) {
 //            data.forEach((item, index) => {
             data.forEach(item => {
                 const li = document.createElement('li');
-                li.innerText = `${item.name}: ${item.time}`;
+                li.innerText = `${item.time}| ${item.name} - ${item.school} gr${item.grade}`;
                 // li.style.borderBottom = "1px solid #eee";
                 // li.style.padding = "4px 0";
                 // li.innerHTML = `<strong>${index + 1}.</strong> ${item.name} (${item.school}) <br> 
